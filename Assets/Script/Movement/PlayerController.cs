72074fc3a41c746b5fbea7158580479e3ddf9d3b
quochuybb <nqhsktt1@gmail.com>
2024-12-02 15:11:10 +0700

    add rotate chracter and bug warning not found

--- Assets/Script/Movement/PlayerController.cs
+++ Assets/Script/Movement/PlayerController.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
     private readonly MoveEvent _moveEvent = new MoveEvent();
     private readonly LookEvent _lookEvent = new LookEvent();
     
--- Assets/Script/Movement/PlayerInput.cs
+++ Assets/Script/Movement/PlayerInput.cs
@@ -20,4 +20,19 @@ public class PlayerInput : PlayerController
         onMoveEvent.Invoke(direction);
         
     }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 direction = value.Get<Vector2>();
+        if (direction.normalized != direction)
+        {
+            Vector2 pointerPosition = _camera.ScreenToWorldPoint(direction);
+            direction = (pointerPosition - (Vector2)transform.position).normalized;
+        }
+
+        if (direction.magnitude >= 0.9f)
+        {
+            onLookEvent.Invoke(direction);
+        }
+    }
 }
