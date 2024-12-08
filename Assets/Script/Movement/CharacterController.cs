69898fc09b47d3d9eadc765a94ad5b4d17662ff3
quochuybb <nqhsktt1@gmail.com>
2024-12-08 15:15:41 +0700

    fix bug error and done movement feature

--- Assets/Script/Movement/CharacterController.cs
+++ Assets/Script/Movement/CharacterController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
     
     private readonly MoveEvent _moveEvent = new MoveEvent();
