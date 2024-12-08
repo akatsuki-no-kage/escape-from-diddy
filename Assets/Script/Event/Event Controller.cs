77a6a71a95d4a59c6d4d2a1bab145d8cae016a99
quochuybb <nqhsktt1@gmail.com>
2024-12-08 22:50:30 +0700

    remove clone folder ad fix scence

--- Assets/Script/Event/Event Controller.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-public class MoveEvent : UnityEvent<Vector2>{}
-public class LookEvent : UnityEvent<Vector2>{}
